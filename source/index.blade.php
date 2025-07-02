7f9d362cf2d14f4bb8f09245da49070157c8ce8a
Alex Gustafson <alex@weareadjacent.com>
2025-07-02 16:38:59 -0400

    Fix html best practices
    Recommendations from phpstorm code analysis:
    * Add alt to image
    * Use correct image dimensions
    * Remove legacy iframe tag attributes

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
 <div class="py-8 mb-8 text-center">
     <h1 class="text-3xl font-bold block">Washbear Chess Club</h1>
     <p>US Chess Federation affiliate in Louisville, KY.</p>
-    <img src="/assets/images/raccoon-with-rook.jpg" width="320" height="320" class="inline-block my-8"/>
+    <img src="/assets/images/raccoon-with-rook-320x320.jpeg" alt="An AI-generated illustration of a raccoon holding a chess piece" width="320" height="320" class="inline-block my-8"/>
 </div>
 <div class="py-8 mb-8 text-center">
     <h2 class="text-3xl font-bold block">Knight Time at Awry</h2>
@@ -21,6 +21,6 @@
     <p>If you're interested in new chess events coming to the Louisville area please signup below.</p>
 </div>
 <div class="bg-gray-100 mx-auto w-full flex justify-center p-4 lg:w-1/2 lg:rounded-xl shadow-lg shadow-gray-500 mb-8">
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSctxUYVUx1AapjRsfS_cHE0ABZwdTLsHEEeaPGjimT2UaATkQ/viewform?embedded=true" width="640" height="2468" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSctxUYVUx1AapjRsfS_cHE0ABZwdTLsHEEeaPGjimT2UaATkQ/viewform?embedded=true" width="640" height="2468">Loading…</iframe>
 </div>
 @endsection
