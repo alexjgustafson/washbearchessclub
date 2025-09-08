b2eabc07ee69c0b99752d409fe0a3ae7aac88c91
Alex Gustafson <alexjgustafson@gmail.com>
2025-09-08 18:40:48 -0400

    add rulebook page

---
+++ source/rulebook.blade.php
@@ -0,0 +1,37 @@
+@extends('_layouts.main')
+
+@section('body')
+<div class="mb-8 text-center">
+    <h2 class="text-3xl font-bold block mb-8">Rulebook</h2>
+    <p>Last updated Sep 8, 2025</p>
+</div>
+<div class="prose mx-auto dark:prose-invert">
+    <h3 id="us-chess">US Chess Official Rules</h3>
+    <p>Tournaments organized by the Washbear Chess Club (WCC) will follow the <em>US Chess Federation's Official Rules of Chess, 7th Edition</em> including its 2025 updates.</p>
+    <p>Select chapters are available for free as a digital download here: <a href="https://new.uschess.org/news/7th-edition-rule-book-free-chapters-updated-2025">https://new.uschess.org/news/7th-edition-rule-book-free-chapters-updated-2025</a>.</p>
+    <p>These free chapters contain most of the rules relevant to players in Washbear Chess Club's over-the-board tournaments.</p>
+    <p>Additions, exceptions, clarifications, or revisions to that rulebook will be listed as "club rules" on this page.</p>
+
+    <h3 id="club-rules">Club Rules</h3>
+
+    <h4 id="late-arrival-forfeit">Late-Arrival Forfeiture</h4>
+    <p>US Chess allows up to one hour (or the game's time control, if less than one hour) of waiting before a non-arriving or late-arriving player forfeits the round by default. WCC shortens this time to 30 minutes unless the player informs the tournament director of their intention to arrive in which case it is extended to one hour.</p>
+
+    <h4 id="cell-phones-and-electronic-devices">Cell Phones and Electronic Devices</h4>
+    <p>Electronic notation devices approved by US Chess are allowed for their intended use.</p>
+    <p>If a player brings any other electronic devices, including cell phones, to the playing area it must be completely turned off.</p>
+    <p>The device must be placed either inside a bag or on the table beside their board for the entirety of the game. If inside a bag, the bag must remain at the board the entirety of the game (not brought to the bathroom, for example.)</p>
+    <p>Consult the tournament director if special accommodations need to be made, for example a medical worker who is "on call."</p>
+
+    <h4 id="reporting-results">Reporting Results</h4>
+    <p>Both players are responsible for reporting the result of their game. Either by marking the pairing sheet or informing the tournament director.</p>
+
+    <h4 id="adjournments">Adjournments</h4>
+    <p>Adjournments will not be used in WCC games.</p>
+    <hr/>
+</div>
+
+<div class="text-center text-xs my-4 block">
+    <a href="/">< washbearchess.club</a>
+</div>
+@endsection
