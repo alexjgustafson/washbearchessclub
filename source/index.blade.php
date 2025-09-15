da92d7c4ccce92358c57e9c28fb93db97d66a688
Alex J. Gustafson <alexjgustafson@gmail.com>
2025-09-14 20:30:24 -0400

    Swap USCF for US Chess

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,7 +7,7 @@
 
 <div class="py-8 mb-8 text-center">
     <h2 class="text-3xl font-bold block">Upcoming Tournaments</h2>
-    <p class="py-2">USCF-rated events</p>
+    <p class="py-2">US Chess Rated Events</p>
     <div class="max-w-2xl mx-auto">
         @include('_molecules.tournament-grid', ['status' => 'upcoming'])
     </div>
