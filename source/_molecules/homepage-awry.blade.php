98f0595f5a4df56c985821577c0a1c330e401154
alexjgustafson <alexjgustafson@gmail.com>
2025-10-11 18:55:13 -0500

    build for deploy

--- source/_molecules/homepage-awry.blade.php
+++ source/_molecules/homepage-awry.blade.php
@@ -11,13 +11,13 @@
 
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mt-8 px-4 md:px-8 lg:px-16">
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/assets/images/smiling-chess-players-at-awry.webp" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/smiling-chess-players-at-awry.webp" alt="Two smiling men playing chess.">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/assets/images/friends-playing-chess.webp" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/friends-playing-chess.webp" alt="A row of three of chess games in progress at a bar.">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/assets/images/casual-chess-in-louisville.webp" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/casual-chess-in-louisville.webp" alt="Four people playing chess at separate tables with brewery tanks and hanging art in the background.">
         </div>
     </div>
 
