ac5939aaa3bce4a5b67b3bc6aa05220df7988990
alexjgustafson <alexjgustafson@gmail.com>
2025-10-11 17:42:06 -0500

    Separate Awry section of homepage to molecule

---
+++ source/_molecules/homepage-awry.blade.php
@@ -0,0 +1,9 @@
+<div class="py-8 mb-8 text-center">
+    <h2 class="text-3xl font-bold block">Knight Time at Awry</h2>
+    <p class="py-2">Casual chess meetup.</p>
+    <p class="py-2"><span class="font-bold">Tuesdays</span> 6pm - 10pm</p>
+    <address class="py-4">
+        <span class="font-bold block">Awry Brewing</span>
+        <span class="block">1025 Barret Ave, Louisville, KY 40204</span>
+    </address>
+</div>
