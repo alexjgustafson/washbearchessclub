dbd4c66923d44039928cca1975e02326ca536618
alexjgustafson <alexjgustafson@gmail.com>
2025-10-11 18:05:51 -0500

    Add gallery to homepage awry section

--- source/_molecules/homepage-awry.blade.php
+++ source/_molecules/homepage-awry.blade.php
@@ -6,4 +6,19 @@
         <span class="font-bold block">Awry Brewing</span>
         <span class="block">1025 Barret Ave, Louisville, KY 40204</span>
     </address>
+
+
+
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mt-8 px-4 md:px-8 lg:px-16">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/smiling-chess-players-at-awry.webp" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/friends-playing-chess.webp" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="/assets/images/casual-chess-in-louisville.webp" alt="">
+        </div>
+    </div>
+
 </div>
