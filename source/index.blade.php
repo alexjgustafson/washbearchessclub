99c092c0c5a3dc60d14a76c81667c2dd1ff328e2
alexjgustafson <alexjgustafson@gmail.com>
2025-10-11 22:00:18 -0500

    improve fb section

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,9 +9,14 @@
 
 @include('_molecules.homepage-awry')
 
-<div class="py-8 mb-8 text-center">
-    <h2 class="text-2xl font-bold accent">Join the Facebook Group</h2>
-    <p><a href="https://www.facebook.com/groups/washbearchessclub">https://www.facebook.com/groups/washbearchessclub</a></p>
+<div
+    style="min-height: 33vh;"
+    class="flex justify-center items-center py-8 mb-8 text-center text-white bg-violet-600 lg:bg-gradient-to-tr lg:from-slate-200 lg:via-violet-600 lg:to-indigo-600">
+    <div class="my-12">
+        <h2 class="mb-4 text-2xl font-bold text-white">Join the Facebook Group</h2>
+        <p>Follow for more photos, event reminders, and updates from other chess events in the Louisville area.</p>
+        <p class="mt-12"><a href="https://www.facebook.com/groups/washbearchessclub" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow Washbear Chess Club on Facebook</a></p>
+    </div>
 </div>
 
 <div class="py-8 mb-8 text-center">
