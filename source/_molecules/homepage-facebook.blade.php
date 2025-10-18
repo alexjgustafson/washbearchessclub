0f57fb0f26d7fcaf6a56c63c3ed124eb0d42fcbc
alexjgustafson <alexjgustafson@gmail.com>
2025-10-18 14:05:41 -0400

    Add more connect options than Facebook

--- source/_molecules/homepage-facebook.blade.php
+++ source/_molecules/homepage-facebook.blade.php
@@ -1,9 +1,18 @@
 <div
         style="min-height: 33vh;"
-        class="flex justify-center items-center py-8 mb-8 text-center text-white bg-violet-600 lg:bg-gradient-to-tr lg:from-slate-200 lg:via-violet-600 lg:to-indigo-600">
+        class="flex justify-center items-center py-8 mb-8 text-center  bg-indigo-300">
     <div class="my-12">
-        <h2 class="mb-4 text-2xl font-bold text-white">Join the Facebook Group</h2>
-        <p>Follow for more photos, event reminders, and updates from other chess events in the Louisville area.</p>
-        <p class="mt-12"><a href="https://www.facebook.com/groups/washbearchessclub" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow Washbear Chess Club on Facebook</a></p>
+        <h2 class="mb-4 text-2xl font-bold ">Connect</h2>
+
+        <p>Join the Discord to chat with club players. Also the best place for any questions about the club.</p>
+        <p class="my-12"><a href="https://discord.gg/UnRcPdm2N6" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join the Discord</a></p>
+
+        <p>The Facebook group has more photos, event reminders, and updates about other chess events in the Louisville area.</p>
+        <p class="my-12"><a href="https://www.facebook.com/groups/washbearchessclub" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow on Facebook</a></p>
+
+        <p>Play games in our Chess.com and Lichess groups.</p>
+        <p class="my-12"><a href="https://www.chess.com/club/washbear-chess-club" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join the Chess.com Group</a></p>
+        <p class="my-12"><a href="https://lichess.org/team/washbear-chess-club" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join the Lichess Team</a></p>
+
     </div>
 </div>
