0669d2a1a426c08dc77f0cd9c9bf3d03b4ca9770
alexjgustafson <alexjgustafson@gmail.com>
2025-11-01 12:09:25 -0400

    QP-001 TBD Listing

---
+++ source/_tournaments/quad-paws-001.blade.php
@@ -0,0 +1,19 @@
+---
+eventId: 4
+title: 'Quad Paws #1'
+gameFormat: 'Quads RR'
+location: 'Louisville, KY'
+entryFee: 'TBD'
+date: 'Feb/Mar 2026'
+---
+
+@extends('_layouts.tournament')
+
+@section('body')
+    <p>"Quads" round robin format. Players will be sorted by rating then divided into four-player round robin sections.
+    Each player will play one game with the other three players in their section.</p>
+    <p>Entry Fee: TBD</p>
+    <p>Prizes: TBD</p>
+    <p>Time control: TBD</p>
+    <p>Location/Schedule/Etc: TBD</p>
+@endsection
