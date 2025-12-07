e87fbe677527987ade89d6fea268b7b2c8b17832
alexjgustafson <alexjgustafson@gmail.com>
2025-12-07 10:17:20 -0500

    About: move uscf history up
    the club history is long now so move
    this link up so it's not lost

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,6 +5,9 @@
         <h2 class="text-3xl font-bold block mb-8">About</h2>
     </div>
     <div class="prose mx-auto dark:prose-invert">
+        <h3>US Chess Federation Affiliate</h3>
+        <p><a target="_blank" href="{!! $page->affiliateMsa !!}">View the club's US Chess Federation tournament history.</a></p>
+
         <h3>Club History</h3>
         <p>In April 2025, Alex Gustafson became a US Chess Club Tournament Director and created Washbear Chess Club in Louisville, KY.</p>
         <p>On July 1st, 2025, the club began its weekly meetup at Awry Brewing that still happens every Tuesday.</p>
@@ -16,9 +19,6 @@
             </p>
             <caption>Members of the 2025 Team Championship team. Left to right: Tyler Mathis, Trey Lemon, Alex Gustafson, and Bryan Miley</caption>
         </figure>
-
-        <h3>US Chess Affiliate</h3>
-        <p><a href="{!! $page->affiliateMsa !!}">View the club's US Chess Federation tournament history.</a></p>
 
         <h3>About Alex</h3>
         <ul>
