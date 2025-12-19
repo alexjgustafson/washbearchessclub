c3ce09c895d290567b1749de82a64450d31ed1da
alexjgustafson <alexjgustafson@gmail.com>
2025-12-18 20:00:17 -0500

    Add IMAGE_BUCKET global and config

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
 
+global $IMAGE_BUCKET;
+$IMAGE_BUCKET = 'https://assets.washbearchess.club/images/';
+
 return [
     'production' => false,
     'baseUrl' => 'washbearchess.club',
@@ -11,7 +14,8 @@ return [
             'sort' => 'eventId'
         ]
     ],
-    'og:image' => 'https://washbearchess.club/assets/images/raccoon-with-rook-320x320.jpeg',
+    'og:image' => 'https://assets.washbearchess.club/images/raccoon-with-rook-320x320.jpeg',
     'affiliateMsa' => 'https://ratings.uschess.org/affiliate/A9630067',
     'currentYear' => date('Y'),
+    'imageBucket' => $IMAGE_BUCKET,
 ];
