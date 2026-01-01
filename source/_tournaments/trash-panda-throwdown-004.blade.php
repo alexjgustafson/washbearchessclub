b7e5a4b0215ca756264149c9a201412ee4a72858
alexjgustafson <alexjgustafson@gmail.com>
2026-01-01 12:56:43 -0500

    Add TPT-004 save the date

---
+++ source/_tournaments/trash-panda-throwdown-004.blade.php
@@ -0,0 +1,17 @@
+---
+eventId: 5
+title: 'Trash Panda Throwdown #4'
+gameFormat: 'Save the Date'
+location: 'Louisville, KY'
+entryFee: ''
+date: 'April 4th/5th, 2026'
+complete: false
+crosstable: ''
+study: ''
+---
+
+@extends('_layouts.tournament')
+@section('body')
+<p>Save the Date! April 4th or 5th, 2026</p>
+<p>More details to come later.</p>
+@endsection
