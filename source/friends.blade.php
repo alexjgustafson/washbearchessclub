7a716e17a3b276622f4596bcfb4cadeebb96a3fd
alexjgustafson <alexjgustafson@gmail.com>
2026-01-19 11:08:44 -0500

    Add friends page

---
+++ source/friends.blade.php
@@ -0,0 +1,32 @@
+@extends('_layouts.main')
+
+@section('body')
+    <div class="mb-8 text-center">
+        <h2 class="text-3xl font-bold block mb-8">Friends of the Washbears</h2>
+    </div>
+    <div class="prose mx-auto dark:prose-invert">
+        <h3>Awry Brewing</h3>
+        <p>Our host every Tuesday since July 2025.</p>
+        <p><a href="https://www.awrybrewing.com">awrybrewing.com</a></p>
+
+        <h3>Finish Line Realty</h3>
+        <p>Scott Hack of Finish Line Realty has donated his Middletown media & training facility for several Washbear events and other community groups.</p>
+        <p><a href="https://www.louisvillerealestate.com">louisvillerealestate.com</a></p>
+
+        <h3>Chess Education Foundation</h3>
+        <p>CEF hosts a tournament every Monday night and a free casual night every Wednesday. Many Washbear members frequent both events. They also host some Washbear Chess Club events.</p>
+        <p><a href="https://chesseducation.org">chesseducation.org</a></p>
+
+        <h3>PB&J Homeschool</h3>
+        <p>Alex coaches a budding chess team for this homeschool co-op.</p>
+        <p><a href="https://www.pbjhomeschool.org">pbjhomeschool.org</a></p>
+
+        <h3>Kentucky Chess Association</h3>
+        <p>KCA is the official US Chess affiliate for Kentucky and allows Washbear Chess Club to post events on its calendar. They're a great resource to find chess events in all parts of the commonwealth.</p>
+        <p><a href="https://kychess.org/open-calendar/">kychess.org</a></p>
+
+        <h3>Chess Earth</h3>
+        <p>A chess community in Cincinnati, Ohio, Chess Earth also maintains one of the most complete calendars of chess events in Ohio, Indiana, and Kentucky. They include Washbear Chess Club events in their calendar.</p>
+        <p><a href="https://chessearth.com/tournament-hall/">chessearth.com</a></p>
+    </div>
+@endsection
