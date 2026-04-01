41930f821d7a96ed0e4eef72b29683b936a2a452
alexjgustafson <alexjgustafson@gmail.com>
2026-04-01 18:08:07 -0400

    tpt-004: add michael johnson

--- source/_tournaments/trash-panda-throwdown-004.blade.php
+++ source/_tournaments/trash-panda-throwdown-004.blade.php
@@ -82,10 +82,15 @@ study: ''
     </p>
 
     <h2>Current Registrations</h2>
-    <p>Last updated Mar 30, 2026. <a href="#online-registration">Add your name to the list!</a></p>
+    <p>Last updated Apr 1, 2026. <a href="#online-registration">Add your name to the list!</a></p>
 
     <h3>Washbear Open</h3>
     @include('_components.registration-table', ['registrations' => [
+        [
+            'name'   => 'Michael C Johnson',
+            'id'     => '12836501',
+            'rating' => '1965',
+        ],
         [
             'name'   => 'Alex Gustafson',
             'id'     => '12598530',
