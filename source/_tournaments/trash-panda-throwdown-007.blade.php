ad2a5616b306511f290557caf3a11dad5bdad693
alexjgustafson <alexjgustafson@gmail.com>
2026-08-28 20:53:33 -0400

    tpt 007 bump date

--- source/_tournaments/trash-panda-throwdown-007.blade.php
+++ source/_tournaments/trash-panda-throwdown-007.blade.php
@@ -82,7 +82,7 @@ study: ''
 
 
     <h2>Current Registrations</h2>
-    <p>Last updated August 26, 2026 8:14am. <a href="#online-registration">Add your name to the list!</a></p>
+    <p>Last updated August 28, 2026 8:53pm. <a href="#online-registration">Add your name to the list!</a></p>
 
     <h3>Washbear Open</h3>
     @include('_components.registration-table', ['registrations' => [
