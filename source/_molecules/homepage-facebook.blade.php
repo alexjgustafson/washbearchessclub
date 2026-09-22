1ec6b4d809f8baff0b2668d7ea29aa9941e611cb
alexjgustafson <alexjgustafson@gmail.com>
2026-09-21 20:35:44 -0400

    Simplify connect section

--- source/_molecules/homepage-facebook.blade.php
+++ source/_molecules/homepage-facebook.blade.php
@@ -10,9 +10,5 @@
         <p>The Facebook group has more photos, event reminders, and updates about other chess events in the Louisville area.</p>
         <p class="my-12"><a href="https://www.facebook.com/groups/washbearchessclub" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Follow on Facebook</a></p>
 
-        <p>Play games in our Chess.com and Lichess groups.</p>
-        <p class="my-12"><a href="https://www.chess.com/club/washbear-chess-club" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join the Chess.com Group</a></p>
-        <p class="my-12"><a href="https://lichess.org/team/washbear-chess-club" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join the Lichess Team</a></p>
-
     </div>
 </div>
